a53fd576a77a03a5be8f32e5075b4c154a26022a
Paulson1112 <jprkpaulson@gmail.com>
2022-01-30 14:48:12 +0530

    test

---
+++ src/test/java/TaskOne.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TaskOne {
+
+public static void main(String[] args) throws IOException {
+	System.out.println("sample");
+	System.out.println("test");
+		}
+		
+ 
+ 
+ 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+
